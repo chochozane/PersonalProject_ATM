5d32aeed073ff86586835c161e71bec99f9bb31a
ZANE <130233619+chochozane@users.noreply.github.com>
2023-12-11 15:03:21 +0900

    [ADD] Add Scripts
    MainButtons 동작 구현

--- Assets/Scripts/Buttons/MainButtons/MainButton_D.cs
+++ Assets/Scripts/Buttons/MainButtons/MainButton_D.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour
+public class MainButton_D : MonoBehaviour
 {
-    public static UIManager instance;
+    //[SerializeField] private Button depositButton;
+    //[SerializeField] private Button withdrawalButton;
 
     [SerializeField] private GameObject mainButtonUI;
     [SerializeField] private GameObject depositButtonUI;
     [SerializeField] private GameObject withdrawalButtonUI;
 
-    private void Awake()
-    {
-        instance = this;
-    }
 
     // Start is called before the first frame update
     void Start()
     {
-        //mainButtonUI.SetActive(true);
-        depositButtonUI.SetActive(false);
-        withdrawalButtonUI.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ClickDepositButton()
+    {
+        mainButtonUI.SetActive(false);
+        depositButtonUI.SetActive(true);
+        withdrawalButtonUI.SetActive(false);
     }
 }
